2eca7e18fb9b9387a3bdb35abea9f17376d27ef2
dicksonmuli <dicksonmully6@gmail.com>
2017-07-03 15:33:18 +0300

    add a cloth model

---
+++ app/models/cloth.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+	name: DS.attr(),
+  price: DS.attr(),
+  type: DS.attr(),
+	gender: DS.attr(),
+  image: DS.attr(),
+  age: DS.attr(),
+  reviews: DS.hasMany('review', {async: true}),
+  material: DS.attr(),
+});
